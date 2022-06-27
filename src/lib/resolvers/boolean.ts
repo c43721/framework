cee1ccc084e0f5352af39c24bef56dc73a8df37b
FC5570 <fellowcoder5570@gmail.com>
2022-06-27 20:52:42 +0530

    feat: switch boolean resolver and argument to v2

--- src/lib/resolvers/boolean.ts
+++ src/lib/resolvers/boolean.ts
@@ -1,16 +1,14 @@
 import { Result } from '@sapphire/result';
 import { Identifiers } from '../errors/Identifiers';
+import { UserError } from '../errors/UserError';
 
 const baseTruths = ['1', 'true', '+', 't', 'yes', 'y'] as const;
 const baseFalses = ['0', 'false', '-', 'f', 'no', 'n'] as const;
 
-export function resolveBoolean(
-	parameter: string,
-	customs?: { truths?: readonly string[]; falses?: readonly string[] }
-): Result<boolean, Identifiers.ArgumentBooleanError> {
+export function resolveBoolean(parameter: string, customs?: { truths?: readonly string[]; falses?: readonly string[] }): Result<boolean, UserError> {
 	const boolean = parameter.toLowerCase();
 	if ([...baseTruths, ...(customs?.truths ?? [])].includes(boolean)) return Result.ok(true);
 	if ([...baseFalses, ...(customs?.falses ?? [])].includes(boolean)) return Result.ok(false);
 
-	return Result.err(Identifiers.ArgumentBooleanError);
+	return Result.err(new UserError({ context: customs, identifier: Identifiers.ArgumentBooleanError }));
 }
